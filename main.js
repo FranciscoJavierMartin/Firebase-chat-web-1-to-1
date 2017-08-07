e21c7a97e33a4a39f9d0cbe2605f2b0ff20edb45
francisco <francisco@localhost.localdomain>
2017-08-08 01:08:41 +0200

    Login funcionality added

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+ ;(function(){
  // Initialize Firebase
   var config = {
     apiKey: "AIzaSyDgPC2Mq_HPQFMZ5WZZd80nF0GnEIDDv0A",
@@ -8,3 +9,32 @@
     messagingSenderId: "301989175636"
   };
   firebase.initializeApp(config);
+
+  var loginBtn=document.getElementById('start-login')
+  var user
+  loginBtn.addEventListener("click",googleLogin)
+
+  function googleLogin(){
+  	var provider = new firebase.auth.GoogleAuthProvider();
+
+  	firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  	user = result.user;
+  	console.log(user)
+  	$("#login").fadeOut()
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+  }
+ })()
+
