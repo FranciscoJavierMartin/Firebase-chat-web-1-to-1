2ee30efc559aebb7dbfeb9b1298e18c3d13e574d
francisco <francisco@localhost.localdomain>
2017-08-08 00:38:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Chat web 1 to 1 with Firebase
+
+This is a chat one to one that use Firebase for storage and authentication
---
+++ main.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDgPC2Mq_HPQFMZ5WZZd80nF0GnEIDDv0A",
+    authDomain: "webchat1to1.firebaseapp.com",
+    databaseURL: "https://webchat1to1.firebaseio.com",
+    projectId: "webchat1to1",
+    storageBucket: "",
+    messagingSenderId: "301989175636"
+  };
+  firebase.initializeApp(config);
